30c509f976e87b3ef5fc8d8c59a00b8a0babadac
nhattanchan <buvghhj123@gmail.com>
2023-09-09 09:17:10 +0700

    commit

---
+++ src/users/dto/input/create-user.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType, Int } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+@InputType()
+export class CreateUserInput {
+
+    @Field()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+
+    @Field(() => Int)
+    @IsNotEmpty()
+    age: number
+
+}
---
+++ src/users/dto/input/update-user.input.ts
@@ -0,0 +1,20 @@
+import { Field, InputType, Int } from "@nestjs/graphql";
+import { IsNotEmpty, IsOptional } from "class-validator";
+
+@InputType()
+export class UpdateUserInput {
+
+    @Field()
+    @IsNotEmpty()
+    userId: string
+
+    @Field(() => Int)
+    @IsOptional()
+    @IsNotEmpty()
+    age: number
+
+    @Field()
+    @IsOptional()
+    isSubscribed?: boolean
+
+}
